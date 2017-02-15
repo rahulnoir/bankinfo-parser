706e8e422556e87e87c05af24bb186ab0b2d4306
rn4ir <rn@rahul-nair.com>
2017-02-14 22:28:16 -0500

    edited src link

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ cur_m_version = sys.version_info[1]
 print ("Current Python version: " + str(cur_M_version) +"."+ str(cur_m_version))
 
 # RBI's source URL, required to download the raw data
-src_url = "https://www.rbi.org.in/Scripts/bs_viewcontent.aspx?Id=2009"
+src_url = ""
 
 # Function takes RBI's URL as input and scraps all the xls links on the page
 def scrape_url (src_url):
