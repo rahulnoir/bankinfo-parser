64b7d921267ef6f21ec96e8301d816535df00947
rn4ir <rn@rahul-nair.com>
2017-03-31 04:08:42 -0400

    added xls_to_csv conversion

---
+++ xls_2_csv/xls_csv_converter.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import xlrd
+import csv
+import os
+
+csv_filepath = "csv/"
+xls_filepath = "xls/"
+
+DEBUG = 1
+
+def create_csvdir():
+    if not os.path.isdir(csv_filepath):
+        if DEBUG == 1:
+            print ("\nDEBUG_MSG: DIR 'csv/' doesn't exist.")
+        os.makedirs(csv_filepath, exist_ok=True)
+    else:
+        if DEBUG == 1:
+            print ("\nDEBUG_MSG: DIR 'csv/' exists.")
+            print ("\nDEBUG_MSG: Deleting 'csv/' DIR.")
+        shutil.rmtree(csv_filepath, ignore_errors=True)
+        os.makedirs(csv_filepath, exist_ok=True)
+
+def xls_to_csv():
+    for xls in os.listdir(xls_filepath):
+        if os.path.isfile(os.path.join(xls_filepath, xls)):
+            xlsfile = xlrd.open_workbook(os.path.join(xls_filepath, xls))
+            xlssheet = xlsfile.sheet_by_index(0)
+            csvfile = open(os.path.join(csv_filepath, (xls + '.csv')), 'w', encoding='utf8')
+            csvwrite = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+            for rownum in range(xlssheet.nrows):
+                csvwrite.writerow(xlssheet.row_values(rownum))
+            csvfile.close()
+            #break
+            #print(xls)
+
